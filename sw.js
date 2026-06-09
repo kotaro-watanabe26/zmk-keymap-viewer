b8a40ed9fc17e8b02cd17b8982220006038250d4
Hoppin bubble <90360735+kotaro-watanabe26@users.noreply.github.com>
2026-06-09 20:33:08 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = "zmk-keymap-viewer-v1";
+const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./app-icon.svg"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        if (response.ok) {
+          const copy = response.clone();
+          caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        }
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(response => {
+        if (response) return response;
+        if (new URL(event.request.url).origin === self.location.origin) return caches.match("./index.html");
+        return new Response("Offline", { status: 503, statusText: "Offline" });
+      }))
+  );
+});
